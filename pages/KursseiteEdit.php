817e4778951c7c31cccedd76b021169d87e49354
Sontek Daniel <sontekdaniel@gmail.com>
2023-07-07 16:07:53 +0200

    KursseiteEdit.php popup-Form added

--- pages/KursseiteEdit.php
+++ pages/KursseiteEdit.php
@@ -33,6 +33,80 @@
 
     </div>
 
+    <form class='popup-form' action="../includes/courseEdit.inc.php" method="post" >
+        <h1>Kurserstellung</h1>
+        <!-- Kursname-Eingabefeld -->
+        <input type='text' id="course_name" name="course_name" placeholder="Kursname eingeben"/>
+        <!-- Fachbereich-Auswahl -->
+        <select id="course_subjectarea" name="course_subjectarea">
+            <option selected="selected">Alle Fachbereiche</option>
+            <option value="B">B (01)</option>
+            <option value="EI">EI (02)</option>
+            <option value="ME">ME (03)</option>
+            <option value="LSE">LSE (04)</option>
+            <option value="GES">GES (05)</option>
+            <option value="MNI">MNI (06)</option>
+            <option value="MND">MND (13)</option>
+        </select>
+        <!-- Semester-Auswahl, wo der Kurs angeboten wird -->
+        <select id="course_semesternumber" name="course_semesternumber">
+            <option selected="selected">Alle Semester</option>
+            <option value="1">1.Semester</option>
+            <option value="2">2.Semester</option>
+            <option value="3">3.Semester</option>
+            <option value="4">4.Semester</option>
+            <option value="5">5.Semester</option>
+        </select>
+        <!-- Semesterzeit-Auswahl -->
+        <div class="rg_course_semestertime">
+            <input type="radio" id="winter" name="course_semestertime" value="winter"><label for="winter">Wintersemester</label>
+            <input type="radio" id="summer" name="course_semestertime" value="summer"><label for="summer">Sommersemester</label>
+        </div>
+        <!-- Einschreibeschlüssel-Eingabe -->
+        <input type="password" id="course_pwd" placeholder="(optional) Einschreibeschlüssel eingeben">
+        <!-- Einschreibeschlüssel-Eingabe -->
+        <button type="submit" id="course_completion_btn">Erstellen</button>
+
+        <button type="button" id="course_completion_cancel_btn"><a href="../pages/mainsite.php">Abbrechen</a></button>
+
+        <?php
+        if (!isset($_GET['error'])){
+        }
+        else{
+            $errorCheck = $_GET['error'];
+            if($errorCheck == "emptyfield"){
+                echo "<p class='error' style='color:red' >You did not fill in all necessary fields!</p>";
+            }
+        }
+
+        if(!isset($_GET['courseCreated'])){
+        }
+        else {
+            $courseCreation = $_GET['courseCreated'];
+            if($courseCreation == 'successful'){
+                echo '<script> 
+               document.addEventListener("DOMContentLoaded", function() {
+         // Disable all clickable elements on the page except for the popup form
+         var elementsToDisable = document.querySelectorAll(
+             "a, li, label, textarea, button, input, img");
+        
+         // Remove the disabled attribute from the elements
+
+         for (var i = 0; i < elementsToDisable.length; i++) {
+                  elementsToDisable[i].removeAttribute("disabled");
+                  elementsToDisable[i].classList.remove("blur-effect");
+              }                 
+ });
+              // Hide the popup form
+              var popupForm = document.querySelector(".popup-form");
+              popupForm.style.visibility = "hidden";
+              popupForm.style.pointerEvents = "none";</script>';
+            }
+        }
+        ?>
+
+    </form>
+
     <div class="BodyCourseClass">
 
 
