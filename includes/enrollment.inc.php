5a4e109ab028d3ce7678438dcac6b83f40fd37e4
Sontek Daniel <sontekdaniel@gmail.com>
2023-07-12 18:19:46 +0200

    enrollment.inc.php 1 row code deleted

--- includes/enrollment.inc.php
+++ includes/enrollment.inc.php
@@ -2,7 +2,6 @@
 include_once "functions.inc.php";
 if (availableCoursePwd($_GET['courseid'])){
     $enrollPw = $_POST['loginKeyPanel'];
-    /*$hashedEnrollPw = password_hash($enrollPw, PASSWORD_DEFAULT );*/
     if (password_verify($enrollPw,getCoursePW($_GET['courseid']))){
         enrollToCourse($_GET['userid'], $_GET['courseid']);
     header("Location: ../pages/KursseiteEdit.php?courseid=" . $_GET["courseid"] . "&enrolled=yes");
